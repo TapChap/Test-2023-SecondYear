2735c822b70d5cbcdb0ac8b65735f121344a6dad
Korozya-bit <shaigimel@gmail.com>
2023-07-24 21:43:52 +0300

    Arm

---
+++ src/ExcaliburTest2023/Arm.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.*;
+
+public class Arm extends SubsystemBase {
+
+    public void toggleGamePieceSetting() {}
+
+    public boolean hasGamePiece() {return false;}
+
+    public boolean isClosed() {return false;}
+
+    public Command collectGamePiece() {return new InstantCommand(()-> {});}
+
+    public Command raiseGamePiece(int height){ return new InstantCommand(()-> {});}
+
+    public Command placeGamePiece(){ return new InstantCommand(()-> {});}
+
+    public Command ejectGamePiece(){ return new InstantCommand(()-> {});}
+}
