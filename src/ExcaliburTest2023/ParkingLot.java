9a0c868b89ae253020f818077347dffbcd3b1143
Korozya-bit <shaigimel@gmail.com>
2023-07-24 21:35:19 +0300

    ParkingLot

---
+++ src/ExcaliburTest2023/ParkingLot.java
@@ -0,0 +1,61 @@
+package ExcaliburTest2023;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ParkingLot {
+    private HashMap<String, Long> cars;
+    private HashMap<String, Integer> paidCars;
+
+    private final int MAX_CAPACITY = 5;
+    private double pricePerHour;
+
+    public ParkingLot(){
+        cars = new HashMap<>();
+        paidCars = new HashMap<>();
+    }
+
+    public boolean hasAvailableSpots(){
+        return cars.size() < MAX_CAPACITY;
+    }
+
+    public void enterCar(String licencePlate){
+        cars.put(licencePlate, getTime());
+    }
+
+    public int getParkingPrice(String licencePlate){
+        return (int) (this.pricePerHour * (getTime() - cars.get(licencePlate)));
+    }
+
+    public void setCarPayed(String licencePlate, int payment){
+        paidCars.put(licencePlate, payment);
+    }
+
+    public boolean hasCarPayed(String licencePlate){
+        return paidCars.containsKey(licencePlate);
+    }
+
+    public void exitCar(String licencePlate){
+        cars.remove(licencePlate);
+        paidCars.remove(licencePlate);
+    }
+
+    public int getParkingLotEarnings(){
+        AtomicInteger sum = new AtomicInteger(0);
+
+        paidCars.forEach((licence, payment) -> sum.addAndGet(payment));
+
+        return sum.get();
+    }
+
+    public void setHourlyPrice(double price){
+        this.pricePerHour = price;
+    }
+
+    public void openBarrier(){
+        System.out.println("The barrier is open");
+    }
+
+    private long getTime(){return Instant.now().getEpochSecond();}
+}
